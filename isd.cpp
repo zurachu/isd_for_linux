8491397043303f4dffb518725cf7b5577efb21bb
zurachu <zurachu@gmail.com>
2018-01-14 22:51:24 +0900

    warning: format specifies type 'int' but the argument has type 'size_t' (aka 'unsigned long') [-Wformat]

--- isd.cpp
+++ isd.cpp
@@ -57,7 +57,7 @@ static int upload( n::piece::Device &d, n::piece::Fs &fs, char *fname )
 static int fs_status( n::piece::Device &d, n::piece::Fs &fs )
 {
 	size_t size = fs.getFreeBlockCount();
-	printf("free_block:%d\nfree_area:%d byte\n", size, size*4096);
+	printf("free_block:%zu\nfree_area:%zu byte\n", size, size*4096);
 	return 0;
 }
 
